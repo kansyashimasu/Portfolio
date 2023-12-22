c12bc529ce33a646ff3f778250bae1f970bbb110
kansyashimasu <guantashi10@gmail.com>
2023-12-22 23:35:34 +0900

    all done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Homepage from './components/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Homepage/>
+    </div>
+  );
+}
+
+export default App;
